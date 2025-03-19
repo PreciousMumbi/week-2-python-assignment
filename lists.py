f5c242e3d559a3da4dee0eeefa3ed263dfd70ec3
PreciousMumbi <mumbiprecious026@gmail.com>
2025-03-20 01:39:45 +0300

    Add files via upload
    my_list

---
+++ lists.py
@@ -0,0 +1,12 @@
+my_list = []
+my_list.append([10,20,30,40]) # Appends a list to the list
+my_list=[10,20,30,40]
+my_list.insert(1,15) # Inserts 15 at index 1
+print(my_list) # Prints the list
+
+my_list.extend([50,60,70]) # Adds more elemetnts to the list
+my_list.remove(70) # Removes 70 from the list
+my_list = [10,20,30,40,50,60]
+my_list.sort() # Sorts the list in ascending order
+print(my_list) # Prints the list
+print(my_list.index(30)) # Prints the index of 30
